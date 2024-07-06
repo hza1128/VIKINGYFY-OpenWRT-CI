e6906ed74817d216718b88213f2c05164788fddf
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-07-06 23:56:23 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -27,7 +27,7 @@ UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "$([[ $WRT_URL =
 
 UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
-UPDATE_PACKAGE "openclash" "vernesong/passwall" "dev"
+UPDATE_PACKAGE "passwall" "vernesong/passwall" "dev"
 
 if [[ $WRT_URL == *"immortalwrt"* ]]; then
 	#UPDATE_PACKAGE "homeproxy" "muink/homeproxy" "dev"
